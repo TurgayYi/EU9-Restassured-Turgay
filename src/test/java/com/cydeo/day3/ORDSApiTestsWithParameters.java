27f2b60eb551d2b3726037e6c37af5ce6b1f929c
CYDEO <tubas2334@gmail.com>
2022-11-18 02:08:15 +0300

    Updated Project

--- src/test/java/com/cydeo/day3/ORDSApiTestsWithParameters.java
+++ src/test/java/com/cydeo/day3/ORDSApiTestsWithParameters.java
@@ -3,6 +3,7 @@ package com.cydeo.day3;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,8 +17,7 @@ public class ORDSApiTestsWithParameters {
     @BeforeAll
     public static void init(){
 
-        baseURI = "http://44.195.19.167:8000";
-
+        baseURI = "http://44.195.19.167:1000/ords/hr";
 
     }
 
@@ -34,12 +34,50 @@ public class ORDSApiTestsWithParameters {
     @Test
     public void test1(){
 
+        Response response= given().accept(ContentType.JSON)
+                .and().queryParam("q","{\"region_id\":2}")
+                .log().all()
+                .when()
+                .get("/countries");
+
+        assertEquals(200,response.statusCode());
+        assertEquals("application/json",response.header("Content-Type"));
+        assertTrue(response.body().asString().contains("United States of America"));
+
+        response.prettyPrint();
+
+
+    }
+
+   /*
+        Send a GET request to employees and get only employees who works as a IT_PROG
+
+     */
+
+    @DisplayName("GET request to /employees with Query Param")
+    @Test
+    public void test2(){
+
+        Response response = given().accept(ContentType.JSON)
+                .and().queryParam("q", "{\"job_id\": \"IT_PROG\"}")
+                .log().all()
+                .when()
+                .get("/employees");
+
+
+        assertEquals(200,response.statusCode());
+        assertEquals("application/json",response.header("Content-Type"));
+        assertEquals("chunked",response.header("Transfer-Encoding"));
+
+
+        assertTrue(response.body().asString().contains("IT_PROG"));
+
+        response.prettyPrint();
 
 
 
 
     }
-
 
 
 

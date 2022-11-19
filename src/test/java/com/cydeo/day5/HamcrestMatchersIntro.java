a61d2d207005cc07da8fb2b541ee912b49081e9f
CYDEO <tubas2334@gmail.com>
2022-11-20 01:14:58 +0300

    updeated

--- src/test/java/com/cydeo/day5/HamcrestMatchersIntro.java
+++ src/test/java/com/cydeo/day5/HamcrestMatchersIntro.java
@@ -5,6 +5,10 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
@@ -60,10 +64,47 @@ public class HamcrestMatchersIntro {
         //endswith
         assertThat(text,endsWithIgnoringCase("Hamcrest"));
 
+        //check if text contains String learning
+        assertThat(text,containsString("learning"));
+
+        //with ignoring case
+        assertThat(text,containsStringIgnoringCase("LEARning"));
+
+       String str = " ";
+        //check if above str is blank
+        assertThat(str,blankString());
+
+        //check if trimmed str is empty string
+        assertThat(str.trim(),emptyString());
+
+
+    }
+
+    @DisplayName("Hamcrest for collection")
+    @Test
+    public void testCollection(){
+
+        List<Integer> listOfNumbers = Arrays.asList(1,4,5,6,32,54,66,77,45,23);
+
+        //check size of the list
+        assertThat(listOfNumbers,hasSize(10));
+
+        //check if this list hasItem 77
+        assertThat(listOfNumbers,hasItem(77));
+
+        //check if this list hasItems 77,54,23
+        assertThat(listOfNumbers,hasItems(77,54,23));
+
+        //check if all numbers greater than 0
+        assertThat(listOfNumbers,everyItem(greaterThan(0)));
 
 
 
 
     }
+
+
+
+
 
 }

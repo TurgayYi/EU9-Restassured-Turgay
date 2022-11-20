b4f8e8e0fd3415db85b29b375a249c41a34c5b97
CYDEO <tubas2334@gmail.com>
2022-11-20 13:53:50 +0300

    added dependency

--- src/test/java/com/cydeo/day5/JasonToJavaTest.java
+++ src/test/java/com/cydeo/day5/JasonToJavaTest.java
@@ -1,7 +1,36 @@
 package com.cydeo.day5;
 
-public class JasonToJavaTest {
+import com.cydeo.utilities.SpartanTestBase;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class JasonToJavaTest extends SpartanTestBase {
+
+    @DisplayName("GET one Spartan and deserialize to Map")
+    @Test
+    public void oneSpartanToMap(){
+
+        Response response = given().accept(ContentType.JSON).and()
+                .pathParam("id", 156)
+                .when().get("/api/spartans/{id}")
+                .then().statusCode(200)
+                .extract().response();
 
 
+        //get the json and convert it to the map
+
+        Map<String,Object> jsonMap = response.as(Map.class);
+
+        System.out.println("jsonMap.toString() = " + jsonMap.toString());
+
+
+    }
 
 }

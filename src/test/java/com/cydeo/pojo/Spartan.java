9b150d35f22207a3985476f505d03a159a45d513
CYDEO <tubas2334@gmail.com>
2022-11-21 18:51:57 +0300

    pojo class added

---
+++ src/test/java/com/cydeo/pojo/Spartan.java
@@ -0,0 +1,53 @@
+package com.cydeo.pojo;
+
+/*
+{
+    "id": 156,
+    "name": "John",
+    "gender": "Male",
+    "phone": 12334987347
+}
+ */
+
+public class Spartan {
+
+    private int id;
+    private String name;
+    private String gender;
+    private long phone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+
+
+}

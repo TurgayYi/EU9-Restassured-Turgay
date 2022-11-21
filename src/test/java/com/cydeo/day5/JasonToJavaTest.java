7fce182b69b166b2c67a1b1cf86e8d77253f93ee
CYDEO <tubas2334@gmail.com>
2022-11-21 16:29:48 +0300

    updated

--- src/test/java/com/cydeo/day5/JasonToJavaTest.java
+++ src/test/java/com/cydeo/day5/JasonToJavaTest.java
@@ -4,12 +4,17 @@ import com.cydeo.utilities.SpartanTestBase;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
 
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class JasonToJavaTest extends SpartanTestBase {
 
@@ -30,7 +35,34 @@ public class JasonToJavaTest extends SpartanTestBase {
 
         System.out.println("jsonMap.toString() = " + jsonMap.toString());
 
+        //after we got the map, we can use hamcrest or junit assertions to do assertion
+        String actualName = (String) jsonMap.get("name");
+        assertThat(actualName,is("John"));
+
+
 
     }
+
+    @DisplayName("GET all spartans to JAVA data structure")
+    @Test
+    public void getAllSpartan(){
+
+        Response response = get("/api/spartans").then().statusCode(200)
+                .extract().response();
+
+        //we need to convert json to java which is deserialize
+
+       List<Map<String,Object>> jsonList =  response.as(List.class);
+
+        System.out.println("jsonList.get(1).get(\"name\") = " + jsonList.get(1).get("name"));
+
+
+        Map<String,Object> spartan3 = jsonList.get(2);
+        System.out.println("spartan3 = " + spartan3);
+
+
+    }
+
+
 
 }
--- src/test/java/com/cydeo/utilities/SpartanTestBase.java
+++ src/test/java/com/cydeo/utilities/SpartanTestBase.java
@@ -1,5 +1,6 @@
 package com.cydeo.utilities;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 import static io.restassured.RestAssured.baseURI;
@@ -9,6 +10,19 @@ public abstract class SpartanTestBase {
     @BeforeAll
     public static void init(){
         baseURI = "http://44.195.19.167:8000";
+
+        String dbUrl = "jdbc:oracle:thin:@44.195.19.167:1521:xe";
+        String dbUsername = "SP";
+        String dbPassword = "SP";
+
+        DBUtils.createConnection(dbUrl,dbUsername,dbPassword);
+
+    }
+
+    @AfterAll
+    public static void tearDown(){
+
+        DBUtils.destroy();
 
     }
 
